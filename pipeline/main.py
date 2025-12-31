3cc437a9b3b30d701a5060fdd4d81407977e377f
Frigonf1 <francoisfrigon99@gmail.com>
2025-12-31 14:52:08 -0500

    Ajout d'un notebook Jupyter avec widget interactif

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,4 +1,5 @@
 from download_data import extract_game_ids
+from transform_data import display_games_data
 from download_data import download_data
 from pathlib import Path
 
@@ -7,3 +8,4 @@ if __name__ == "__main__":
     year = 2022
     save_path = Path(f"data/nhl_play_by_play_{team_abbr}_{year}_{year+1}")
     download_data(team_abbr, year, save_path)
+    display_games_data(extract_game_ids(team_abbr, year))
