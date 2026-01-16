6db616da02fb0d441273fdcbb56f16ec9db2cad2
Frigonf1 <francoisfrigon99@gmail.com>
2026-01-16 16:17:27 -0500

    All teams, one season

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from pipeline.download_data import download_data, create_shots_dataframe
 from data_viz.data_plots import plot_shot_totals, plot_shot_heatmap 
+from nhlpy import NHLClient
 from pathlib import Path
 import json
 
 if __name__ == "__main__":
-    team_abbr = "MTL" 
-    years = [2022, 2023, 2024] 
 
-    for year in years:
+    client = NHLClient()
+
+    teams = client.teams.teams()
+
+    # Extract team abbreviations
+    teams_abbr = [team['abbr'] for team in teams]
+    print(teams_abbr)
+
+    year = 2024 
+
+    for team_abbr in teams_abbr:
         save_path = Path(f"data/nhl_play_by_play_{team_abbr}_{year}_{year+1}.json")
         save_path.parent.mkdir(parents=True, exist_ok=True)
         download_data(team_abbr, year, save_path)
@@ -19,4 +28,5 @@ if __name__ == "__main__":
         print(shots_df.head())
     
         # plot_shot_totals(shots_df, team_abbr, year)
-        plot_shot_heatmap(shots_df, team_abbr, year)
+    
+    plot_shot_heatmap(shots_df, team_abbr, year)
